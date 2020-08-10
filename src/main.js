13acd56d39f6f0416f24140a7285bab72e7117ed
Qq01 <swistakaqq01@gmail.com>
2020-08-10 14:28:35 +0200

    Added examples Added PartialRenderer Removed logs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './lib/codemirror/codemirror.js';
 import './lib/codemirror/mode/javascript/javascript.js';
 import { PaintScript } from './paint-script.js';
-const paintScript = new PaintScript(document.body);
+const container = document.createElement('div');
+document.body.appendChild(container);
+const paintScript = new PaintScript(container);
--- src/paint-script.js
+++ src/paint-script.js
@@ -14,37 +14,43 @@ function elem(tagName, ...childs) {
 function text(txt) {
     return document.createTextNode(txt);
 }
-function makeTabs() {
+function makeTabs(addTabBtn = true, ...tabsNames) {
     const tabClass = 'ps-tabs__tab';
     const tabSelectedClass = 'ps-tabs__tab--selected';
     const addTabClass = 'ps-tabs__add-tab';
-    function addTab({dispatchEvents = true} = {}) {
-        console.log('dispatch', dispatchEvents);
+    function addTab({dispatchEvents = true, name} = {}) {
         const index = $tabs.tabCount++;
-        const $tab = elem('div', text(index));
+        const $tab = elem('div', text(name ? name : index));
+        $tab.dataset.name = name ? name : index;
         $tab.dataset.tabIndex = index;
         $tab.classList.add(tabClass);
         $tab.onclick = function(){selectTab(index)};
-        $tabs.lastChild.insertAdjacentElement('beforebegin', $tab);
+        if (addTabBtn) {
+            $tabs.lastChild.insertAdjacentElement('beforebegin', $tab);
+        } else {
+            $tabs.appendChild($tab);
+        }
         if (dispatchEvents) {
             $tabs.dispatchEvent(new CustomEvent('tabs:add', {detail: {index}}));
         }
         selectTab(index, {dispatchEvents});
     }
     function selectTab(index, {dispatchEvents = true} = {}) {
-        const prevTab = $tabs.querySelector(`.${tabSelectedClass}`);
-        if (prevTab) {
-            prevTab.classList.remove(tabSelectedClass);
+        const $prevTab = $tabs.querySelector(`.${tabSelectedClass}`);
+        if ($prevTab) {
+            $prevTab.classList.remove(tabSelectedClass);
         }
-        const nextTab = $tabs.querySelector(`.${tabClass}[data-tab-index="${index}"]`);
-        if (nextTab) {
-            nextTab.classList.add(tabSelectedClass);
+        const $nextTab = $tabs.querySelector(`.${tabClass}[data-tab-index="${index}"]`);
+        if ($nextTab) {
+            $nextTab.classList.add(tabSelectedClass);
             $tabs.selectedTabIndex = index;
         }
         if (dispatchEvents) {
             $tabs.dispatchEvent(new CustomEvent('tabs:change', {detail: {
-                prevTabIndex: prevTab && prevTab.dataset.tabIndex ? Number.parseInt(prevTab.dataset.tabIndex) : null,
-                nextTabIndex: nextTab ? index : null
+                prevTabIndex: $prevTab && $prevTab.dataset.tabIndex ? Number.parseInt($prevTab.dataset.tabIndex) : null,
+                nextTabIndex: $nextTab ? index : null,
+                prevTabName: $prevTab?.dataset.name,
+                nextTabName: $nextTab.dataset.name
             }}));
         }
     }
@@ -55,16 +61,24 @@ function makeTabs() {
             $tabs.removeChild($t);
         });
     }
-    const $addTab = elem('div', text('Add'));
-    $addTab.onclick = addTab;
-    $addTab.classList.add(addTabClass, tabClass);
-    const $tabs = elem('div', $addTab);
+    const $tabs = elem('div');
+    if (addTabBtn) {
+        const $addTab = elem('div', text('Add'));
+        $addTab.onclick = addTab;
+        $addTab.classList.add(addTabClass, tabClass);
+        $tabs.appendChild($addTab);
+    }
     $tabs.classList.add('ps-tabs');
     $tabs.addTab = e => addTab({dispatchEvents: false});
     $tabs.tabCount = 0;
     $tabs.selectedTabIndex = 0;
-    addTab();
+    if (tabsNames.length) {
+        tabsNames.forEach(name => addTab({name}));
+    } else {
+        addTab();
+    }                
     $tabs.clearTabs = clearTabs;
+    $tabs.selectTab = selectTab;
     return $tabs;
 }
 function makeEditor() {
@@ -83,7 +97,11 @@ loop(() => {
 
     const $editorContainer = elem('div');
     $editorContainer.classList.add('ps-editor-container');
-    const codeMirror = CodeMirror($editorContainer, {lineNumbers: true, value: defaultFunctionText});
+    const codeMirror = CodeMirror($editorContainer, {
+        lineNumbers: true,
+        value: defaultFunctionText,
+        viewportMargin: Infinity
+    });
     $editorContainer.editor = codeMirror;
     return $editorContainer;
 }
@@ -110,6 +128,74 @@ function makeActionbar() {
     return $ab;
 }
 
+const scriptExamples = [
+{
+    title: 'UV to RGB',
+    data:`//UV to RGB
+ps.resizeView(256, 256);//resizing canvas.
+loop(() => {// loop executes passed function once per frame.
+    let time = performance.now();
+    let b = Math.sin(time * 0.001);
+    ps.eachPixel(data => {// function is executed per pixel.
+        //color values passed to function are normalized.
+        //data.u and data.v are normalized x nad y coords.
+        //not normalized pixel position is held in data.x and data.y
+        data.rgba[0] = data.u;
+        data.rgba[1] = data.v;
+        data.rgba[2] = b;
+        data.rgba[3] = 1;//setting alpha channel to 1 so its no longer transparent.
+    });
+});`
+},
+{
+    title: 'PartialRenderer for large canvas',
+    data: `//Script No. 0
+ps.resizeView(1000, 1000);
+const r = ps.partialRenderer();//getting instance of PartialRenderer
+r.setDivision(19);//Ammount of frames needed to fully render image.
+//the bigger value the more visible artefact can be.
+//for best results set value to number that is not divisible by image width
+loop(() => {
+    let time = performance.now();
+    let b = Math.sin(time * 0.001);
+    const img = ps.getImageData();//get image data from canvas
+    r.setImageData(img);//set image data for that we want to process
+    r.eachPixel(data => {//execute function only for 1/19 of canvas pixels
+        data.rgba[0] = data.u;
+        data.rgba[1] = data.v;
+        data.rgba[2] = b;
+        data.rgba[3] = 1;
+    });
+    ps.putImageData(img);//putting back resulted image data to our canvas.
+    r.next();//tell PartialRenderer that we finished rendering in current frame so in next iteration 
+});`
+}
+];
+function makeExampleList() {
+    const $exampleList = elem('div');
+    $exampleList.classList.add('ps-example-list');
+    $exampleList.style.display = 'none';
+    scriptExamples.forEach(example => {
+        const $ex = elem('div');
+        $ex.classList.add('ps-examplelist__example-button');
+        $ex.innerHTML = example.title;
+        $ex.onclick = e => {
+            $exampleList.dispatchEvent(new CustomEvent('example-list:selected', {detail: {
+                ...example
+            }}));
+        }
+        $exampleList.appendChild($ex);
+    });
+    return $exampleList;
+}
+
+function makeResourceContainer() {
+    const $resourceContainer = elem('div');
+    $resourceContainer.classList.add('ps-resource__container');
+    $resourceContainer.style.display = 'none';
+    return $resourceContainer;
+}
+
 export class PaintScript {
     /**
      * @type {PaintScriptAPI}
@@ -122,6 +208,7 @@ export class PaintScript {
     constructor($root) {
         this.#api = new PaintScriptAPI(this);
         this.$root = $root;
+        this.$root.classList.add('ps-container');
         this.scripts = [];
         this.actions = [];
         this.storage = {};
@@ -133,10 +220,20 @@ export class PaintScript {
         this.$root.appendChild(this.$editorContainer);
         this.$editorContainer.editor.refresh();
         this.scripts.push(this.$editorContainer.editor.getDoc());
+        this.$exampleList = makeExampleList();
+        this.$root.appendChild(this.$exampleList);
         this.$canvas = makeCanvas();
         this.width = this.$canvas.width;
         this.height = this.$canvas.height;
-        this.$root.appendChild(this.$canvas);
+        this.$appTabs = makeTabs(false, 'Canvas', 'Resources', 'Examples');
+        this.$appTabs.classList.add('ps-tabs--app-tabs');
+        this.$appTabs.selectTab(0);
+        this.$root.appendChild(this.$appTabs);
+        const $canvasContainer = elem('div', this.$canvas)
+        $canvasContainer.classList.add('ps-canvas-container');
+        this.$root.appendChild($canvasContainer);
+        this.$resourceContainer = makeResourceContainer();
+        this.$root.appendChild(this.$resourceContainer);
         /**
          * @type {CanvasRenderingContext2D}
          */
@@ -149,6 +246,30 @@ export class PaintScript {
             if (e.detail.nextTabIndex != null) {
                 this.$editorContainer.editor.swapDoc(this.scripts[e.detail.nextTabIndex]);
                 this.$editorContainer.editor.refresh();
+            }
+        });
+        this.$appTabs.addEventListener('tabs:change', e => {
+            switch (e.detail.nextTabName) {
+                case 'Canvas':
+                    $canvasContainer.style.display = '';
+                    break;
+                case 'Resources':
+                    this.$resourceContainer.style.display = '';
+                    break;
+                case 'Examples':
+                    this.$exampleList.style.display = '';
+                    break;
+            }
+            switch (e.detail.prevTabName) {
+                case 'Canvas':
+                    $canvasContainer.style.display = 'none';
+                    break;
+                case 'Resources':
+                    this.$resourceContainer.style.display = 'none';
+                    break;
+                case 'Examples':
+                    this.$exampleList.style.display = 'none';
+                    break;
             }
         });
 
@@ -194,17 +315,23 @@ export class PaintScript {
                 let i = 0;
                 let lastDoc;
                 while (i < count) {
-                    console.log('loop', i, '<', count);
                     this.$tabs.addTab();
-                    console.log('added tab');
                     const text = localStorage.getItem(`document_${i}`);
-                    console.log(text);
                     lastDoc = this.scripts[i] = new CodeMirror.Doc(text, "javascript");
                     i++;
                 }
                 if (lastDoc) {
                     this.$editorContainer.editor.swapDoc(lastDoc);
                 }
+            }
+        });
+        this.$exampleList.addEventListener('example-list:selected', e => {
+            if (confirm('Override project with example data?')) {
+                this.scripts = [];
+                this.$tabs.clearTabs();
+                this.$tabs.addTab();
+                this.scripts.push(new CodeMirror.Doc(e.detail.data, "javascript"));
+                this.$editorContainer.editor.swapDoc(this.scripts[0]);
             }
         });
     }
@@ -224,6 +351,50 @@ export class PaintScript {
     resizeView = (width, height) => {
         this.width = this.$canvas.width = width;
         this.height = this.$canvas.height = height;
+    }
+}
+
+class PartialRenderer {
+    #division = 1;
+    #imageData = null;
+    #offset = 0;
+    setDivision = division => {
+        this.#division = division;
+        this.#offset = 0;
+    }
+    setImageData = imageData => {
+        this.#imageData = imageData;
+    }
+    next = () => {
+        this.#offset++;
+        if (this.#offset >= this.#division) {
+            this.#offset = 0;
+        }
+    }
+    eachPixel = fn => {
+        const img = this.#imageData;
+        let x = 0;
+        let y = 0;
+        for (let i = this.#offset * 4; i < img.data.length; i += this.#division * 4) {
+            x = (i/4) % img.width;
+            y = Math.floor((i/4)  / img.width);
+            let rgba = Array.from(img.data.slice(i, i + 4));
+            rgba = rgba.map(v => v / 255)
+            let data = {
+                rgba,
+                x,
+                y,
+                u: x / img.width,
+                v: y / img.height
+            };
+            fn(data);
+            let j = (x + y * img.width) * 4;
+            data.rgba = data.rgba.map(v => Math.min(255, Math.max(0, Math.round(v * 255))));
+            img.data[j] = data.rgba[0];
+            img.data[j + 1] = data.rgba[1];
+            img.data[j + 2] = data.rgba[2];
+            img.data[j + 3] = data.rgba[3];
+        }
     }
 }
 
@@ -269,4 +440,22 @@ class PaintScriptAPI {
         }
         this.ctx.putImageData(img, 0, 0);
     }
+    getImageData() {
+        return this.ctx.getImageData(0, 0, this.#ps.width, this.#ps.height);
+    }
+    putImageData(img) {
+        this.ctx.putImageData(img, 0, 0);
+    }
+    partialRenderer() {
+        return new PartialRenderer();
+    }
+}
+
+class ResourceStorage {
+    data = new Set();
+}
+class Resource {
+    name = 'File';
+    ext = 'txt';
+    data = null;
 }
